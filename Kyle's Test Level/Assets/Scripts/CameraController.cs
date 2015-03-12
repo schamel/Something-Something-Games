ead372af0d564df767af9703de0c21ef9c3036a7
Kyle Johnson <kajwwd@mst.edu>
2015-03-12 02:02:45 -0500

    Added A Second Test Level
    Second Test Level Added.  Also, Dash Rings and Springs will now set the
    player to their centers before launching them.

---
+++ Kyle's Test Level/Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject target;
+	public float leashRange;
+	//public float followTightness;
+
+	private Vector3 _startOffset;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_startOffset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	// LateUpdate is called after all other update functions
+	void LateUpdate ()
+	{
+		moveCam ();
+	}
+
+	/// <summary>
+	/// Moves the camera.  This version acts as if the camera is "leashed" to the player, only moving when the player gets too far away.
+	/// The class member <paramref name="leashRange"/> determines how far the player can go before the camera is moved.
+	/// </summary>
+	private void moveCam ()
+	{
+		Vector2 currentPos = new Vector2 (transform.position.x, transform.position.y);
+		Vector2 targetPos = new Vector2 (target.transform.position.x, target.transform.position.y);
+		Vector2 offset = targetPos - currentPos;
+
+		if (offset.magnitude > leashRange) {
+			offset = offset.normalized * (offset.magnitude - leashRange);
+
+			transform.position = new Vector3 (currentPos.x + offset.x, currentPos.y + offset.y, _startOffset.z);
+		}
+	}
+
+	/// <summary>
+	/// Moves the camera.  This version keeps the camera constantly moving towards the player at a rate that is proportional to the value of
+	/// the class member <paramref name="followTightness"/>.
+	/// </summary>
+	/*
+	private void moveCam() {
+		Vector2 currentPos = new Vector2 (transform.position.x, transform.position.y);
+		Vector2 targetPos = new Vector2 (target.transform.position.x, target.transform.position.y);
+		Vector2 offset = targetPos - currentPos;
+
+		offset *= followTightness;
+
+		transform.position = new Vector3(currentPos.x + offset.x, currentPos.y + offset.y, _startOffset.z);
+	}
+	*/
+}
---
+++ Kyle's Test Level/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody2D _rigidBody;
+	public float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	// FixedUpdate is called once per fixed frame, and should be used for physics calculations
+	void FixedUpdate ()
+	{
+		float hInput = Input.GetAxis ("Horizontal");
+		_rigidBody.AddForce (new Vector2 (hInput * speed * Time.deltaTime, 0));
+	}
+
+	void OnTriggerEnter2D (Collider2D trigger)
+	{
+		switch (trigger.gameObject.tag) {
+		case "Respawn":
+			transform.position = new Vector2 (0, 0.5f);
+			_rigidBody.velocity = new Vector2 (_rigidBody.velocity.x, 0);
+			break;
+		}
+	}
+}
