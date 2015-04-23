ad2c04ab5ee2c65065eb3e7d9b6a8258e694769f
Kyle Johnson <kajwwd@mst.edu>
2015-04-23 01:39:32 -0500

    Added A Tiled Test Level
    Made a full fledged level using Yarbrough's method.  You can get through
    it, but it's nowhere near as smooth as it needs to be.

---
+++ Kyle's Test Level/Assets/Scripts/PseudoOneWayFloor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PseudoOneWayFloor : MonoBehaviour 
+{
+	private Collider2D platform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		platform = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			platform.enabled = !(platform.enabled);
+		}
+		
+	}
+}
+
