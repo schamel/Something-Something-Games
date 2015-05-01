ecb17d85738350c945b65b3590bdb92dbfdf23e9
Kyle Johnson <kajwwd@mst.edu>
2015-05-01 00:10:11 -0500

    I May Have Forgot Some Things
    Just resolving issues, mainly.  Also, Alex, you'll probably have to
    re-add the tooltips on the jump pads.

--- Kyle's Test Level/Assets/Scripts/Player.cs
+++ Kyle's Test Level/Assets/Scripts/Player.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 	private Rigidbody2D _rigidBody;
 	public float speed;
-	public float jumpf = 350f;
-	public LayerMask myMask;
-	bool grounded;
-	Vector2 groundpt;
 
 	bool grounded = false;
 	public Transform groundCheck;
@@ -26,33 +21,19 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-<<<<<<< HEAD
-	
-=======
 		if (grounded && Input.GetKeyDown(KeyCode.Space)) 
 		{
 			_rigidBody.AddForce(new Vector2(0, jumpForce));
 		}
->>>>>>> origin/master
 	}
 
 	// FixedUpdate is called once per fixed frame, and should be used for physics calculations
 	void FixedUpdate ()
 	{
-<<<<<<< HEAD
-		groundpt.x = transform.position.x;
-		groundpt.y = transform.position.y - 0.66f;
-		grounded = Physics2D.Linecast (transform.position, groundpt, myMask);
-=======
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
->>>>>>> origin/master
 
 		float hInput = Input.GetAxis ("Horizontal");
 		_rigidBody.AddForce (new Vector2 (hInput * speed * Time.deltaTime, 0));
-
-		if(Input.GetButtonDown ("Jump") && grounded){
-			_rigidBody.AddForce(Vector2.up * jumpf);
-		}
 	}
 
 	void OnTriggerEnter2D (Collider2D trigger)
