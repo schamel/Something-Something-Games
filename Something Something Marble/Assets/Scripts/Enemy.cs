599150dbdad84a6352c90e23b93a09a510df673f
Blitzsinger <bmlxw2@mst.edu>
2015-05-03 22:01:27 -0500

    Aggresive bat
    He is spazzie and annoying but i love him

--- Something Something Marble/Assets/Scripts/Enemy.cs
+++ Something Something Marble/Assets/Scripts/Enemy.cs
@@ -4,34 +4,37 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 	private float health=40;
 	private Vector3 _startOffset;
-	float temp;
-	float dist;
-	float offset;
-	float cero;
-	int direction;
+	//private float temp;
+	private float dist;
+	private float offset;
+	private float cero;
+	private int direction;
+	public bool chasing;
 	// Use this for initialization
 	void Start () {
-		temp = transform.position.y;
+		//temp = transform.position.y;
 		dist = 0;
 		offset = .01f;
 		cero = 0;
 		direction = 1;
+		chasing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (health <= 0)
 			Destroy (gameObject);
-		
-		if (dist > 100 || dist < 0) {
-			offset = offset * (-1);
-			direction = direction * (-1);
-			dist = dist + direction;
-		} else {
-			temp = temp + offset;
-			dist = dist + direction;
+		if (!chasing) {
+			if (dist > 100 || dist < 0) {
+				offset = offset * (-1);
+				direction = direction * (-1); 
+				dist = dist + direction;
+			} else {
+				//temp = temp + offset;
+				dist = dist + direction;
+			}
+			transform.position = new Vector3 (transform.position.x, transform.position.y + offset, cero);
 		}
-		transform.position = new Vector3(transform.position.x, temp, cero);
 	
 	}
 	void OnTriggerEnter2D (Collider2D other)
